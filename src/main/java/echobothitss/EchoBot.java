e6fc9492707dce5ba52f45075c89458aa91eb60c
quel502 <96394591+quel502@users.noreply.github.com>
2022-01-03 14:45:29 -0300

    Update EchoBot.java

--- src/main/java/echobothitss/EchoBot.java
+++ src/main/java/echobothitss/EchoBot.java
@@ -26,7 +26,7 @@ public class EchoBot extends ActivityHandler {
         String teamsChannelId = turnContext.getActivity().teamsGetChannelId();
         System.out.print(teamsChannelId); 
         return turnContext.sendActivity(
-             MessageFactory.text("Teste Echo: " + turnContext.getActivity().getText() + teamsChannelId)
+            MessageFactory.text(teamsChannelId + "Echo: " + turnContext.getActivity().getText())
         ).thenApply(sendResult -> null);
     }
 
